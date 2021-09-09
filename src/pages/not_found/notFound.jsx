deb7a02377f3041e3228493b8078e0076f97983e
Svitlana <devop.edu@gmail.com>
2021-09-09 20:50:19 +0200

    implemented 404 page

--- src/pages/not_found/notFound.jsx
+++ src/pages/not_found/notFound.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import styles from "./notFound.module.css";
 import PageLayout from "../../components/layout/PageLayout";
+import notFoundImage from "../../assets/images/notFound.png";
 
 const notFound = () => {
 	return (
 		<PageLayout>
-			<h1>sorry, page not found</h1>
+			<div className={styles.wrapper}>
+				<img src={notFoundImage} alt="page not found" />
+			</div>
+
+			<Link to={"/"} className={styles.button}>
+				To Home Page
+			</Link>
 		</PageLayout>
 	);
 };
