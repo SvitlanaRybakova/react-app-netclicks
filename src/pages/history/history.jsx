aaac6e31b036850bbbc3cdc82fdc6a7e1c822f4e
Svitlana <devop.edu@gmail.com>
2021-09-14 15:39:24 +0200

    simple layout for History page

--- src/pages/history/history.jsx
+++ src/pages/history/history.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PageLayout from "../../components/layout/PageLayout";
 import Title from "../../components/title/Title";
+import useLocalStorage from "../../hooks/useLocalStorage";
 
 const history = () => {
+	const [storedValue, setValue] = useLocalStorage();
+	console.log(storedValue);
 	return (
 		<>
 			<Title title={"History"} />
 			<PageLayout>
-				<h1>History Page</h1>
+				<h3 className="text-center m-5">Here are the last movies where you have seen</h3>
+
+				{storedValue &&
+					storedValue.map((link, index) => (
+						<p className="m-3" key={link.id}>
+							<Link to={link.url}>Movie # {index} </Link>
+						</p>
+					))}
 			</PageLayout>
 		</>
 	);
