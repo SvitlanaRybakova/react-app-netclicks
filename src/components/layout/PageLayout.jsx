59e8e65fe542f4a9b0b2efa5011ff47fdcdec4e5
Svitlana <devop.edu@gmail.com>
2021-09-16 13:58:50 +0200

    add themoviedb warning in the footer

--- src/components/layout/PageLayout.jsx
+++ src/components/layout/PageLayout.jsx
@@ -3,9 +3,18 @@ import styles from "./PageLayout.module.css";
 
 const PageLayout = ({ children }) => {
 	return (
-		<div className={styles.wrapper}>
-			<div className="container">{children}</div>
-		</div>
+		<>
+			<div className={styles.wrapper}>
+				<div className="container">{children}</div>
+				<div className="footer">
+					<p>
+						{" "}
+						This product uses the TMDB API but is not endorsed or
+						certified by TMDB.
+					</p>
+				</div>
+			</div>
+		</>
 	);
 };
 
