0b1c78cb673d45e3900e69a13891a2a6c0782672
Svitlana <devop.edu@gmail.com>
2021-10-11 20:52:30 +0200

    changed ui of history page

--- src/pages/history/history.jsx
+++ src/pages/history/history.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
 import { v4 as uuidv4 } from "uuid";
 
 import PageLayout from "../../components/layout/PageLayout";
 import Title from "../../components/title/Title";
 import BackButton from "../../components/back_button/BackButton";
 import useLocalStorage from "../../hooks/useLocalStorage";
+import HistoryCard from "./historyCard/HistoryCard";
 
 const history = () => {
 	const [storedValue, setValue] = useLocalStorage();
+
 	return (
 		<>
 			<Title title={"History"} />
@@ -18,12 +21,12 @@ const history = () => {
 					Here are the last movies where you have seen
 				</h3>
 
-				{storedValue &&
-					storedValue.map((link, index) => (
-						<p className="m-3" key={uuidv4()}>
-							<Link to={link.url}>Movie # {index + 1} </Link>
-						</p>
-					))}
+				<div style={{ display: "flex", flexWrap:"wrap" }}>
+					{storedValue &&
+						storedValue.map((link, index) => (
+							<HistoryCard key={uuidv4()} id={link.id} />
+						))}
+				</div>
 			</PageLayout>
 		</>
 	);
---
+++ src/pages/history/historyCard/HistoryCard.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { useQuery } from "react-query";
+import { useHistory } from "react-router-dom";
+
+import styles from "../../../components/movie_card/MovieCard.module.css";
+import Image from "../../../components/image/Image";
+import CustomErrorMessage from "../../../components/error_message/CustomErrorMessage";
+import Spinner from "../../../components/spinner/Spinner";
+import { getMovieById } from "../../../services/ThemoviedbAPI";
+
+const HistoryCard = ({ id }) => {
+	const { data, error, isError, isLoading } = useQuery(
+		["history-card", id],
+		() => getMovieById(id),
+		{
+			staleTime: 1000 * 60 * 5, // 5 mins
+			cacheTime: 1000 * 60 * 30, // 30 mins
+			keepPreviousData: true, // keep previous data
+		}
+	);
+  let history = useHistory();
+  
+  if(data){
+    console.log(data);
+  }
+	return (
+		<>
+			{isError && <CustomErrorMessage error={error} />}
+			{isLoading && <Spinner />}
+			{data && (
+				<div
+					className={styles.tvShows__item}
+					onClick={() => history.push(`/movies/${id}`)}
+				>
+					<div className={styles.tvCard}>
+						<span className={styles.tvCard__vote}>
+							{data.vote_average}
+						</span>
+						<Image
+							poster={data.poster_path}
+							title={data.title}
+						/>
+						<h4 className={styles.tvCard__head}>{data.title}</h4>
+					</div>
+				</div>
+			)}
+		</>
+	);
+};
+
+export default HistoryCard;
